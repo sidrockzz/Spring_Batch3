7b250e2c69029b5d89bc0ad32565aad37de52948
sidrockzz <marshtomp.1999@hotmail.com>
2021-05-22 19:25:30 +0530

    Conditional flow with custom

--- src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
+++ src/main/java/com/linkedin/batch/LinkedinBatchApplication.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.flow.JobExecutionDecider;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
@@ -13,11 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
 
 @SpringBootApplication
 @EnableBatchProcessing
@@ -30,82 +26,96 @@ public class LinkedinBatchApplication {
 	public StepBuilderFactory stepBuilderFactory;
 
 	@Bean
-	public Step storePackageStep(){
-		return this.stepBuilderFactory
-				.get("storePackageStep")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Storing the package while the customer address is located");
-						return RepeatStatus.FINISHED;
-					}
-				}).build();
+	public JobExecutionDecider decider() {
+		return new DeliveryDecider();
 	}
 
 	@Bean
-	public Step givePackageToCustomerStep(){
-		return this.stepBuilderFactory
-				.get("givePackageToCustomerStep")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-						System.out.println("Given Package to the customer.");
-						return RepeatStatus.FINISHED;
-					}
-				}).build();
+	public Step leaveAtDoorStep() {
+		return this.stepBuilderFactory.get("leaveAtDoorStep").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Leaving the package at the door.");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
 
 	@Bean
-	public Step driveToAddressStep(){
+	public Step storePackageStep() {
+		return this.stepBuilderFactory.get("storePackageStep").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Storing the package while the customer address is located.");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+
+	@Bean
+	public Step givePackageToCustomerStep() {
+		return this.stepBuilderFactory.get("givePackageToCustomer").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				System.out.println("Given the package to the customer.");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
+	}
+
+	@Bean
+	public Step driveToAddressStep() {
 
 		boolean GOT_LOST = false;
-		return this.stepBuilderFactory
-				.get("driveToAddressStep")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-						if(GOT_LOST){
-							throw new RuntimeException("Got lost driving to the address");
-						}
-						System.out.println("Successfully arrived at the address.");
-						return RepeatStatus.FINISHED;
-					}
-				}).build();
+		return this.stepBuilderFactory.get("driveToAddressStep").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+
+				if(GOT_LOST) {
+					throw new RuntimeException("Got lost driving to the address");
+				}
+
+				System.out.println("Successfully arrived at the address.");
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
 
 	@Bean
 	public Step packageItemStep() {
-		return this.stepBuilderFactory
-				.get("packageItemStep")
-				.tasklet(new Tasklet() {
-					@Override
-					public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-						String item= chunkContext.getStepContext().getJobParameters().get("item").toString();
-						//String date= chunkContext.getStepContext().getJobParameters().get("run.date").toString();
-						Date date = Calendar.getInstance().getTime();
-						DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
-						String strDate = dateFormat.format(date);
-						System.out.println("The "+item+ " has been package on "+strDate+".");
-						return RepeatStatus.FINISHED;
-					}
-				})
-                .build();
+		return this.stepBuilderFactory.get("packageItemStep").tasklet(new Tasklet() {
+
+			@Override
+			public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+				String item = chunkContext.getStepContext().getJobParameters().get("item").toString();
+				String date = chunkContext.getStepContext().getJobParameters().get("run.date").toString();
+
+				System.out.println(String.format("The %s has been packaged on %s.", item, date));
+				return RepeatStatus.FINISHED;
+			}
+		}).build();
 	}
 
 	@Bean
-	public Job deliverPackageJob(){
-		return this.jobBuilderFactory
-				.get("deliveryPackageJob")
+	public Job deliverPackageJob() {
+		return this.jobBuilderFactory.get("deliverPackageJob")
 				.start(packageItemStep())
 				.next(driveToAddressStep())
-				.on("FAILED").
-						to(storePackageStep())
+				.on("FAILED").to(storePackageStep())
 				.from(driveToAddressStep())
-				.on("*")
-				.to(givePackageToCustomerStep())
+				.on("*").to(decider())
+				.on("PRESENT").to(givePackageToCustomerStep())
+				.from(decider())
+				.on("NOT_PRESENT").to(leaveAtDoorStep())
 				.end()
 				.build();
 	}
+
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(LinkedinBatchApplication.class, args);
